a32f5cd7118e6a9d03290c198aad99e39b54f104
Mykhailo Ulanovych <97468374+UlanovychM@users.noreply.github.com>
2024-01-11 20:28:34 +0200

    end

--- app.js
+++ app.js
@@ -1551,3 +1551,197 @@ promise
 	.then(value => console.log(value)) // "Success! Value passed to resolve function"
 	.catch(error => console.log(error)) // "Error! Error passed to reject function"
 	.finally(() => console.log('Promise settled')); // "Promise settled"
+
+const promise2 = new Promise((resolve, reject) => {
+	setTimeout(() => {
+		resolve(5);
+	}, 2000);
+});
+
+promise2
+	.then(value => {
+		console.log(value); // 5
+		return value * 2;
+	})
+	.then(value => {
+		console.log(value); // 10
+		return value * 3;
+	})
+	.then(value => {
+		console.log(value); // 30
+	})
+	.catch(error => {
+		console.log(error);
+	})
+	.finally(() => {
+		console.log('finally');
+	});
+
+// const fetchUserFromServer = (username, onSuccess, onError) => {
+// 	console.log(`Fetching data for ${username}`);
+
+// 	setTimeout(() => {
+// 		// Change value of isSuccess variable to simulate request status
+// 		const isSuccess = true;
+
+// 		if (isSuccess) {
+// 			onSuccess('success value');
+// 		} else {
+// 			onError('error');
+// 		}
+// 	}, 2000);
+// };
+
+// fetchUserFromServer(
+// 	'Mango',
+// 	user => console.log(user),
+// 	error => console.error(error)
+// );
+
+const fetchUserFromServer = username => {
+	return new Promise((resolve, reject) => {
+		console.log(`Fetching data for ${username}`);
+
+		setTimeout(() => {
+			// Change value of isSuccess variable to simulate request status
+			const isSuccess = true;
+
+			if (isSuccess) {
+				resolve('success value'); // значенням параметра resolve буде колбек-функція методу then()
+			} else {
+				reject('error'); // значенням параметра reject буде колбек-функція методу catch()
+			}
+		}, 2000);
+	});
+};
+
+const userPromise = fetchUserFromServer('Mango'); // результатом виклику fetchUserFromServer("Mango") буде проміс
+
+// проміс обробляємо у методах then() i catch()
+userPromise
+	.then(user => console.log(user))
+	.catch(error => console.error(error));
+
+new Promise(resolve => resolve('success value'))
+	.then(value => console.log(value))
+	.catch(error => console.log(error));
+
+// Так створюється успішно виконаний проміс через Promise.resolve():
+
+Promise.resolve('success value')
+	.then(value => console.log(value))
+	.catch(error => console.log(error));
+
+// Так створюється проміс, що виконався з помилкою через new Promise():
+
+new Promise((resolve, reject) => reject('error'))
+	.then(value => console.log(value))
+	.catch(error => console.log(error));
+
+// Так створюється проміс, що виконався з помилкою через Promise.reject():
+
+Promise.reject('error')
+	.then(value => console.log(value))
+	.catch(error => console.log(error));
+
+// Промисифікація
+
+// const makeGreeting = guestName => {
+// 	return new Promise((resolve, reject) => {
+// 		if (!guestName) {
+// 			reject('Guest name must not be empty');
+// 		} else {
+// 			resolve(`Welcome ${guestName}`);
+// 		}
+// 	});
+// };
+
+// makeGreeting('Mango')
+// 	.then(greeting => console.log(greeting))
+// 	.catch(error => console.error(error));
+
+
+// Metodth .resolve() .reject()
+
+const makeGreeting = guestName => {
+	if (!guestName) {
+		return Promise.reject('Guest name must not be empty');
+	} else {
+		return Promise.resolve(`Welcome ${guestName}`);
+	}
+};
+
+makeGreeting('Mango')
+	.then(greeting => console.log(greeting))
+	.catch(error => console.error(error));
+
+new Promise((resolve, reject) => {
+	setTimeout(() => {
+		resolve('Fulfilled A');
+	}, 1000);
+})
+	.then(value => console.log(value))
+	.catch(error => console.log(error));
+
+new Promise((resolve, reject) => {
+	setTimeout(() => {
+		resolve('Fulfilled B');
+	}, 3000);
+})
+	.then(value => console.log(value))
+	.catch(error => console.log(error));
+
+new Promise((resolve, reject) => {
+	setTimeout(() => {
+		reject('Rejected C');
+	}, 2000);
+})
+	.then(value => console.log(value))
+	.catch(error => console.log(error)); // "Rejected C"
+
+const makePromise = ({ value, delay, shouldResolve = true }) => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			if (shouldResolve) {
+				resolve(value);
+			} else {
+				reject(value);
+			}
+		}, delay);
+	});
+};
+
+makePromise({ value: 'A', delay: 1000 })
+	.then(value => console.log(value)) // "A"
+	.catch(error => console.log(error));
+
+makePromise({ value: 'B', delay: 3000 })
+	.then(value => console.log(value)) // "B"
+	.catch(error => console.log(error));
+
+makePromise({ value: 'C', delay: 2000, shouldResolve: false })
+	.then(value => console.log(value))
+	.catch(error => console.log(error)); // "C"
+
+const p1 = Promise.resolve(1);
+const p2 = Promise.resolve(2);
+const p3 = Promise.resolve(3);
+
+// .all()
+
+Promise.all([p1, p2, p3])
+	.then(values => console.log(values)) // [1, 2, 3]
+	.catch(error => console.log(error));
+
+// const p1 = Promise.resolve(1);
+// const p2 = Promise.reject('Rejected promise 2');
+// const p3 = Promise.resolve(3);
+ 
+// .allSettled()
+
+Promise.allSettled([p1, p2, p3]).then(values => console.log(values));
+// [
+//   { status: "fulfilled", value: 1},
+//   { status: "rejected", value: "Rejected promise 2"},
+//   { status: "fulfilled", value: 3}
+// ]
