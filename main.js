81d5ac512c3937bab23057f50fb06768f113e25c
Mykhailo Ulanovych <97468374+UlanovychM@users.noreply.github.com>
2024-07-23 20:34:54 +0300

    update

--- main.js
+++ main.js
@@ -1907,7 +1907,7 @@ console.log(salary * hour);
 
 // // Axios
 
-import axios from 'axios';
+// import axios from 'axios';
 
 // axios.defaults.baseURL = '<https://jsonplaceholder.typicode.com>';
 
@@ -1977,64 +1977,82 @@ import axios from 'axios';
 // 	console.log('Posts: ', response.data);
 // };
 
-const fetchPostsBtn = document.querySelector('.btn');
-const postList = document.querySelector('.posts');
+// const fetchPostsBtn = document.querySelector('.btn');
+// const postList = document.querySelector('.posts');
 
-// Controls the group number
-let page = 1;
-// Controls the number of items in the group
-let perPage = 10;
+// // Controls the group number
+// let page = 1;
+// // Controls the number of items in the group
+// let perPage = 10;
 
-fetchPostsBtn.addEventListener('click', async () => {
-	try {
-		const posts = await fetchPosts();
-		renderPosts(posts);
-		// Increase the group number
-		page += 1;
+// fetchPostsBtn.addEventListener('click', async () => {
+// 	try {
+// 		const posts = await fetchPosts();
+// 		renderPosts(posts);
+// 		// Increase the group number
+// 		page += 1;
 
-		// Replace button text after first request
-		if (page > 1) {
-			fetchPostsBtn.textContent = 'Fetch more posts';
-		}
-	} catch (error) {
-		console.log(error);
-	}
-});
+// 		// Replace button text after first request
+// 		if (page > 1) {
+// 			fetchPostsBtn.textContent = 'Fetch more posts';
+// 		}
+// 	} catch (error) {
+// 		console.log(error);
+// 	}
+// });
 
-async function fetchPosts() {
-	const params = new URLSearchParams({
-		_limit: perPage,
-		_page: page,
-	});
+// async function fetchPosts() {
+// 	const params = new URLSearchParams({
+// 		_limit: perPage,
+// 		_page: page,
+// 	});
 
-	const response = await axios.get(
-		`https://jsonplaceholder.typicode.com/posts?${params}`
-	);
-	return response.data;
+// 	const response = await axios.get(
+// 		`https://jsonplaceholder.typicode.com/posts?${params}`
+// 	);
+// 	return response.data;
+// }
+
+// function renderPosts(posts) {
+// 	const markup = posts
+// 		.map(({ id, title, body, userId }) => {
+// 			return `<li>
+//           <h2 class="post-title">${title.slice(0, 30)}</h2>
+//           <p><b>Post id</b>: ${id}</p>
+//           <p><b>Author id</b>: ${userId}</p>
+//           <p class="post-body">${body}</p>
+//         </li>`;
+// 		})
+// 		.join('');
+// 	postList.insertAdjacentHTML('beforeend', markup);
+// }
+
+// // Контролює кількість елементів в групі
+// let limit = 30;
+// // Кількість груп в колекції
+// const totalPages = Math.ceil(100 / limit);
+
+// if (page > totalPages) {
+// 	return iziToast.error({
+// 		position: 'topRight',
+// 		message: "We're sorry, there are no more posts to load",
+// 	});
+// }
+
+// let start = prompt('how are u');
+
+// let b = prompt('how are 2');
+
+// let c = prompt('how are 3');
+
+// console.log('aaaa' + start + b + c);
+
+const kg = +prompt('Enter kg');
+const height = +prompt('Enter height');
+
+let bmi = kg / Math.pow(height, 2);
+
+if (bmi >= 25) {
+	alert(` u overweight not normal ${bmi - 25}`);
 }
-
-function renderPosts(posts) {
-	const markup = posts
-		.map(({ id, title, body, userId }) => {
-			return `<li>
-          <h2 class="post-title">${title.slice(0, 30)}</h2>
-          <p><b>Post id</b>: ${id}</p>
-          <p><b>Author id</b>: ${userId}</p>
-          <p class="post-body">${body}</p>
-        </li>`;
-		})
-		.join('');
-	postList.insertAdjacentHTML('beforeend', markup);
-}
-
-// Контролює кількість елементів в групі
-let limit = 30;
-// Кількість груп в колекції
-const totalPages = Math.ceil(100 / limit);
-
-if (page > totalPages) {
-	return iziToast.error({
-		position: 'topRight',
-		message: "We're sorry, there are no more posts to load",
-	});
-}
+alert(`u overweight normal`);
