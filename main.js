abb0d3f1d1760a7de94faf8126f6d05506fb25c6
Mykhailo <97468374+UlanovychM@users.noreply.github.com>
2024-09-28 20:48:50 +0300

    update

--- main.js
+++ main.js
@@ -2296,3 +2296,43 @@ for (const odd of odds) {
 const average = sum / odds.length;
 
 console.log(average);
+
+// Write a program that takes a list of variable names written in underscore_case and converts them to camelCase.
+// Input will come from a textarea html element inserted into the DOM (see code below to insert elements) and the transformation will happen when the button is clicked.
+
+// Test data (for inserting into the textarea including spaces):
+
+// underscore_case
+//   first_name
+// next_Variable
+//    Calculate_AGE
+// arrived_flight
+
+// This output should appear (5 separate console.log outputs):
+// underscoreCase
+// firstName
+// nextVariable
+// calculateAge
+// arrivedFlight
+
+// Hints:
+// 1.	Remember which character defines a newline in a textbox
+// 2.	The solution should only work for a two-word variable like first_second
+// 3.	This is a tricky task, so start watching the solution in case you get stuck. Then pause and continue!
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+document.querySelector('button').addEventListener('click', () => {
+	const text = document.querySelector('textarea').value;
+	const lines = text.split('\n');
+
+	for (const line in lines) {
+		const [first, second] = line.toLowerCase().trim().split('_');
+		const output = `${first}${second.replace(
+			second[0],
+			second[0].toUpperCase()
+		)}`;
+		console.log(output);
+	}
+});
